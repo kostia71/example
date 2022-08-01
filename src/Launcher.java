a397818e45c25ad05138c991d77df1e093312506
Kostia Chaplyha <kostia71@gmail.com>
2022-08-01 12:01:48 +0300

    Initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,9 @@
+/**
+ * @author Kostya
+ * @link https://github.com/kostia71/tic-tac-toe-2.git
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
